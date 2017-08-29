04d13ef99f29f673f9b5ef109193fd8cff1fbc33
arodrigu46 <30849124+arodrigu46@users.noreply.github.com>
2017-08-28 20:34:19 -0500

    validaciones
    Validaciones de combinacion realizadas, tanto en código como en  interfaz

--- app/src/main/java/com/xyz/arodrigu46/xyzenterprise/Principal.java
+++ app/src/main/java/com/xyz/arodrigu46/xyzenterprise/Principal.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,11 +21,17 @@ public class Principal extends AppCompatActivity {
     private String dij[];
     private Spinner tipo;
     private String tip[];
+    private RadioButton rbcop;
+    private RadioButton rbusd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        rbcop = (RadioButton) findViewById(R.id.rbcop);
+        rbusd = (RadioButton) findViewById(R.id.rbusd);
+
         txtUnidades = (EditText) findViewById(R.id.txtUnidades);
         res = (TextView) findViewById(R.id.txtValor);
         resources  = this.getResources();
@@ -46,12 +53,108 @@ public class Principal extends AppCompatActivity {
 
 
     }
-    public void calcular(View v){
+    public void calcular(View v) {
 
-        if(validar()){
-            res.setText("yes");
-        }else{
-            res.setText("not");
+        if (validar()) {
+            Double resultado;
+            Double unidades = Double.parseDouble(txtUnidades.getText().toString());
+            int coin=1;
+            if(rbcop.isChecked()==true){
+                coin = 1;
+            }if(rbusd.isChecked()==true){
+                coin=3200;
+            }
+            int pmat = material.getSelectedItemPosition();
+            int pdij = dije.getSelectedItemPosition();
+            int ptip = tipo.getSelectedItemPosition();
+            if(pmat==0 && pdij==1 && (ptip==0 || ptip==2)){
+
+
+                resultado = (unidades*320000/coin);
+                res.setText(String.valueOf(resultado));
+
+            }
+            if(pmat==0 && pdij==1 && ptip==3){
+
+
+                resultado = (unidades*256000/coin);
+                res.setText(String.valueOf(resultado));
+
+            }
+            if(pmat==0 && pdij==1 && ptip==4){
+
+
+                resultado = (unidades*224000/coin);
+                res.setText(String.valueOf(resultado));
+
+            }
+            if(pmat==0 && pdij==0 && (ptip==0 || ptip==2)){
+
+
+                resultado = (unidades*384000/coin);
+                res.setText(String.valueOf(resultado));
+
+            }
+            if(pmat==0 && pdij==0 && ptip==3){
+
+
+                resultado = (unidades*320000/coin);
+                res.setText(String.valueOf(resultado));
+
+            }
+            if(pmat==0 && pdij==0 && ptip==4){
+
+
+                resultado = (unidades*288000/coin);
+                res.setText(String.valueOf(resultado));
+
+            }
+            if(pmat==1 && pdij==1 && (ptip==0 || ptip==2)){
+
+
+                resultado = (unidades*288000/coin);
+                res.setText(String.valueOf(resultado));
+
+            }
+            if(pmat==1 && pdij==1 && ptip==3){
+
+
+                resultado = (unidades*224000/coin);
+                res.setText(String.valueOf(resultado));
+
+            }
+            if(pmat==1 && pdij==1 && ptip==4){
+
+
+                resultado = (unidades*160000/coin);
+                res.setText(String.valueOf(resultado));
+
+            }
+            if(pmat==1 && pdij==0 && (ptip==0 || ptip==2)){
+
+
+                resultado = (unidades*352000/coin);
+                res.setText(String.valueOf(resultado));
+
+            }
+            if(pmat==1 && pdij==0 && ptip==3){
+
+
+                resultado = (unidades*288000/coin);
+                res.setText(String.valueOf(resultado));
+
+            }
+            if(pmat==1 && pdij==0 && ptip==4){
+
+
+                resultado = (unidades*256000/coin);
+                res.setText(String.valueOf(resultado));
+
+            }
+            if(ptip==1){
+                Toast.makeText(this, resources.getString(R.string.error02),Toast.LENGTH_SHORT).show();
+            }
+
         }
     }
     public boolean validar(){
@@ -60,7 +163,12 @@ public class Principal extends AppCompatActivity {
             return false;
         }
         else{
-            return true;
+            if(rbcop.isChecked()!=true && rbusd.isChecked()!=true){
+                Toast.makeText(this, resources.getString(R.string.error01),Toast.LENGTH_SHORT).show();
+                return false;
+            }else{
+                return true;
+            }
         }
     }
 }
